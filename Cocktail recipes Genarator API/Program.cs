c6c2eccc39d1f839489567573ab2aa96ec04817c
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-02-12 14:31:42 +0530

    update Final

--- Cocktail recipes Genarator API/Program.cs
+++ Cocktail recipes Genarator API/Program.cs
@@ -1,21 +1,18 @@
-﻿var baseAddress = "https://www.thecocktaildb.com/api/json/v1/1/";
+﻿using System.Text.Json;
+
+var baseAddress = "https://www.thecocktaildb.com/api/json/v1/1/";
 var requistUri = "search.php?s=margarita";
+Console.WriteLine("\tCocktail recipes Genarator..! (API = Cocktail Database)");
+Console.WriteLine("Waiting for loading....\n");
+IApiDataReader apiDataReader = new ApiDataReader();
+var json =await apiDataReader.Read(baseAddress, requistUri);
+var root = JsonSerializer.Deserialize<Root>(json);
+var count = 0;
 
-
+foreach (var item in root.drinks)
+{
+    Console.WriteLine($"{count+1}.Drink Name :{item.strDrink},\nCatogory :{item.strCategory},\nAlcohole :{item.strAlcoholic},\nIntroductions :{item.strInstructionsDE},\nIngredient : {item.strIngredient1},{item.strIngredient2},{item.strIngredient3},{item.strIngredient4},{item.strIngredient5},{item.strIngredient6}");
+    Console.WriteLine();
+    count++;
+}
 Console.ReadLine();
-public interface IApiDataReader
-{
-    public Task<string> Read(string baseAddress, string requistUri);
-}
-public class ApiDataReader : IApiDataReader
-{
-    public async Task<string>Read(string baseAddress, string requistUri)
-    {
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(baseAddress);
-        HttpResponseMessage responce = await client.GetAsync(requistUri);
-        responce.EnsureSuccessStatusCode();
-        var json =await responce.Content.ReadAsStringAsync();
-        return json;
-    }
-}
---
+++ Cocktail recipes Genarator API/Root.cs
@@ -0,0 +1,5 @@
+﻿using System.Text.Json.Serialization;
+
+public record Root(
+    [property: JsonPropertyName("drinks")] IReadOnlyList<Drink> drinks
+);
